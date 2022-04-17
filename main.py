89112ac45d5f13a21da27f3085ca78e59ffc8832
MikeKennedyDev <Mike.Kennedy.Dev@gmail.com>
2022-04-16 20:45:03 -0400

    working

--- main.py
+++ main.py
@@ -10,15 +10,7 @@ if __name__ == '__main__':
         playlist_tracks = Spotify.Core.GetAllPlaylistTrackIds(spotify)
         liked_tracks = Spotify.Core.GetLikedTrackIds(spotify)
 
-        unique_tracks = set(playlist_tracks + liked_tracks)
-
-        pause = 0
-
-        t1 = list(unique_tracks)
-
-        spotify.playlist_add_items(library_playlist_id, t1[10])
-
-        pause = 0
+        Spotify.Core.WriteToLibrary(spotify, library_playlist_id, (playlist_tracks + liked_tracks))
 
     except BaseException as err:
         print('Some error')
